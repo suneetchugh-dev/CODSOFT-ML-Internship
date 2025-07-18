32f8a25562f5753e7d8746e0e1bf1e1496c80875
Suneet Chugh <76564569+suneetchugh-dev@users.noreply.github.com>
2025-07-18 23:06:23 +0530

    Update churn_prediction.py

--- Task-3_Customer-Churn/churn_prediction.py
+++ Task-3_Customer-Churn/churn_prediction.py
@@ -1,1 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+df = pd.read_csv("Customer_Churn.csv")
+df.drop(['customerID'], axis=1, inplace=True)
+df.replace(" ", np.nan, inplace=True)
+df['TotalCharges'] = df['TotalCharges'].astype(float)
+df.dropna(inplace=True)
+
+cols = df.select_dtypes(include=['object']).columns
+for col in cols:
+    if df[col].nunique() == 2:
+        df[col] = LabelEncoder().fit_transform(df[col])
+    else:
+        df = pd.get_dummies(df, columns=[col])
+
+X = df.drop('Churn', axis=1)
+y = df['Churn']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(6,4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=["No Churn", "Churn"], yticklabels=["No Churn", "Churn"])
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.tight_layout()
+plt.show()
 
